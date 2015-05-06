3a50f65c81a3b7fc99a7418082fb00eb8850c287
Kuniwak <yuki.kokubun@mixi.co.jp>
2015-05-07 08:57:05 +0900

    Make jscs happy

--- gulpfile.js
+++ gulpfile.js
@@ -11,16 +11,18 @@ var PEG_FILES = 'peg_src/**/*.pegjs';
 var COMPILED_PEG_FILE = 'peg_lib';
 
 gulp.task('watch', function() {
-  gulp.src(PEG_FILES).
-    pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') })).
-    pipe(watch(PEG_FILES)).
-    pipe(peg().on('error', gutil.log)).
-    pipe(notify('Compiled: <%= file.relative %>!')).
-    pipe(gulp.dest(COMPILED_PEG_FILE));
+  return gulp.src(PEG_FILES)
+    .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
+    .pipe(watch(PEG_FILES))
+    .pipe(peg().on('error', gutil.log))
+    .pipe(notify('Compiled: <%= file.relative %>!'))
+    .pipe(gulp.dest(COMPILED_PEG_FILE));
 });
 
-gulp.task('build:peg', function() {
-  gulp.src(PEG_FILES).
-    pipe(peg().on('error', gutil.log)).
-    pipe(gulp.dest(COMPILED_PEG_FILE));
+gulp.task('peg', function() {
+  return gulp.src(PEG_FILES)
+    .pipe(peg().on('error', gutil.log))
+    .pipe(gulp.dest(COMPILED_PEG_FILE));
 });
+
+gulp.task('build', ['peg']);
