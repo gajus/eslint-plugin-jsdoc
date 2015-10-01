752c8777b24f5e0230eaa68b61e9fa357e651ac9
Gajus Kuizinas <gk@anuary.com>
2015-10-02 00:46:02 +0100

    require-returns-description is now added. All references to return are renamed to returns.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,40 @@
 import checkParamNames from './rules/checkParamNames';
 import checkRedundantParams from './rules/checkRedundantParams';
 import checkRedundantReturns from './rules/checkRedundantReturns';
-import checkReturnTypes from './rules/checkReturnTypes';
+import checkReturnsTypes from './rules/checkReturnsTypes';
 import newlineAfterDescription from './rules/newlineAfterDescription';
 import requireDescriptionCompleteSentence from './rules/requireDescriptionCompleteSentence';
 import requireParam from './rules/requireParam';
 import requireParamDescription from './rules/requireParamDescription';
 import requireParamTypes from './rules/requireParamTypes';
-import requireReturnTypes from './rules/requireReturnTypes';
+import requireReturnsDescription from './rules/requireReturnsDescription';
+import requireReturnsTypes from './rules/requireReturnsTypes';
 
 export default {
     rules: {
         'check-param-names': checkParamNames,
         'check-redundant-params': checkRedundantParams,
         'check-redundant-returns': checkRedundantReturns,
-        'check-return-types': checkReturnTypes,
+        'check-returns-types': checkReturnsTypes,
         'newline-after-description': newlineAfterDescription,
         'require-description-complete-sentence': requireDescriptionCompleteSentence,
         'require-param': requireParam,
         'require-param-description': requireParamDescription,
         'require-param-types': requireParamTypes,
-        'require-return-types': requireReturnTypes,
+        'require-returns-description': requireReturnsDescription,
+        'require-returns-types': requireReturnsTypes,
     },
     rulesConfig: {
         'check-param-names': 0,
         'check-redundant-params': 0,
         'check-redundant-returns': 0,
-        'check-return-types': 0,
+        'check-returns-types': 0,
         'newline-after-description': 0,
         'require-description-complete-sentence': 0,
         'require-param': 0,
         'require-param-description': 0,
         'require-param-types': 0,
-        'require-return-types': 0,
+        'require-returns-description': 0,
+        'require-returns-types': 0
     }
 };
