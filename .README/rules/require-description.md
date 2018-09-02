17784466abad7460f9b1dadb4a0e8660d7575977
Anthony Elcocks <ajelcocks@gmail.com>
2018-09-02 02:18:23 -0400

    feat: add require-description (#91)
    * add rule require-description
    
    * correct a typo

---
+++ .README/rules/require-description.md
@@ -0,0 +1,13 @@
+### `require-description`
+
+Requires that all functions have a description.
+
+* All functions must have a `@description` tag.
+* Every description tag must have a non-empty description that explains the purpose of the method.
+
+|||
+|---|---|
+|Context|`ArrowFunctionExpression`, `FunctionDeclaration`, `FunctionExpression`|
+|Tags|`class`, `example`|
+
+<!-- assertions requireDescription -->
---
+++ test/rules/assertions/requireDescription.js
@@ -0,0 +1,74 @@
+/* eslint-disable no-restricted-syntax */
+
+export default {
+  invalid: [
+    {
+      code: `
+          /**
+           *
+           */
+          function quux () {
+
+          }
+      `,
+      errors: [
+        {
+          message: 'Missing JSDoc @description declaration.'
+        }
+      ]
+    },
+    {
+      code: `
+          /**
+           * @description
+           */
+          function quux () {
+
+          }
+      `,
+      errors: [
+        {
+          message: 'Missing JSDoc @description description.'
+        }
+      ]
+    }
+  ],
+  valid: [
+    {
+      code: `
+          /**
+           * @description
+           * // arbitrary description content
+           */
+          function quux () {
+
+          }
+      `
+    },
+    {
+      code: `
+          /**
+           * @description
+           * quux(); // does something useful
+           */
+          function quux () {
+
+          }
+      `
+    },
+    {
+      code: `
+          /**
+           * @description <caption>Valid usage</caption>
+           * quux(); // does something useful
+           *
+           * @description <caption>Invalid usage</caption>
+           * quux('random unwanted arg'); // results in an error
+           */
+          function quux () {
+
+          }
+      `
+    }
+  ]
+};
