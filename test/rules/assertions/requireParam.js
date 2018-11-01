7898aee106c6dfb93676b9e321477efafdf4664c
Brett Zamir <brettz9@yahoo.com>
2018-11-01 18:18:14 +0800

    - Enhancement: Add new boolean options `allowImplementsWithoutParam` and `allowAugmentsExtendsWithoutParam` and apply to `requireParam` (fixes #100)

--- test/rules/assertions/requireParam.js
+++ test/rules/assertions/requireParam.js
@@ -68,6 +68,51 @@ export default {
           message: 'Missing JSDoc @param "foo" declaration.'
         }
       ]
+    },
+    {
+      code: `
+          /**
+           * @implements
+           */
+          function quux (foo) {
+
+          }
+      `,
+      errors: [
+        {
+          message: 'Missing JSDoc @param "foo" declaration.'
+        }
+      ]
+    },
+    {
+      code: `
+          /**
+           * @augments
+           */
+          function quux (foo) {
+
+          }
+      `,
+      errors: [
+        {
+          message: 'Missing JSDoc @param "foo" declaration.'
+        }
+      ]
+    },
+    {
+      code: `
+          /**
+           * @extends
+           */
+          function quux (foo) {
+
+          }
+      `,
+      errors: [
+        {
+          message: 'Missing JSDoc @param "foo" declaration.'
+        }
+      ]
     }
   ],
   valid: [
@@ -133,6 +178,84 @@ export default {
           allowOverrideWithoutParam: true
         }
       }
+    },
+    {
+      code: `
+          /**
+           * @implements
+           */
+          function quux (foo) {
+
+          }
+      `,
+      settings: {
+        jsdoc: {
+          allowImplementsWithoutParam: true
+        }
+      }
+    },
+    {
+      code: `
+          /**
+           * @implements
+           * @param foo
+           */
+          function quux (foo) {
+
+          }
+      `
+    },
+    {
+      code: `
+          /**
+           * @augments
+           */
+          function quux (foo) {
+
+          }
+      `,
+      settings: {
+        jsdoc: {
+          allowAugmentsExtendsWithoutParam: true
+        }
+      }
+    },
+    {
+      code: `
+          /**
+           * @augments
+           * @param foo
+           */
+          function quux (foo) {
+
+          }
+      `
+    },
+    {
+      code: `
+          /**
+           * @extends
+           */
+          function quux (foo) {
+
+          }
+      `,
+      settings: {
+        jsdoc: {
+          allowAugmentsExtendsWithoutParam: true
+        }
+      }
+    },
+    {
+      code: `
+          /**
+           * @extends
+           * @param foo
+           */
+          function quux (foo) {
+
+          }
+      `
     }
   ]
 };
