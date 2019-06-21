142ac178ce84632d5a8e0f6ee1e03e10d7c14d41
Brett Zamir <brettz9@yahoo.com>
2019-06-21 11:33:14 +0800

    fix: add flat-map-polyfill to dependencies and use in `no-undefined-types` in place of (unpublished and large) core-js

--- src/rules/noUndefinedTypes.js
+++ src/rules/noUndefinedTypes.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-unassigned-import
-import 'core-js/stable/array/flat-map';
+import 'flat-map-polyfill';
 import _ from 'lodash';
 import {parse as parseType, traverse} from 'jsdoctypeparser';
 import iterateJsdoc, {parseComment} from '../iterateJsdoc';
