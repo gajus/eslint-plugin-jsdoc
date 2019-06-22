4befa80b0f1bdf0126b1511834c30a58222bb1b0
Brett Zamir <brettz9@yahoo.com>
2019-06-22 19:50:22 +0800

    chore: update `eslint` in `peerDependencies` to 6.0.0

--- .ncurc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // Whitelist all for checking besides `peer` which indicates
-  //   somewhat older versions of `eslint` we still support even
-  //   while our devDeps point to a more recent version
-  "dep": "prod,dev,optional,bundle"
-};
