6de2256f25df68bfa110d014e373dbea81ed017d
ahwayakchih <ahwayakchih@gmail.com>
2019-09-30 14:07:47 +0200

    fix(`check-indentation`): ignore example code blocks
    This adds an `excludeExamples` option, defaulting to `true`.
    When enabled (or not configured) it will mask all `@example` tags
    and their content before testing indentation.
    That prevents errors about indentation inside example code
    blocks and keeps correct line numbers when reporting indentation error
    found elsewhere.
    
    It should fix #334.

--- src/rules/checkIndentation.js
+++ src/rules/checkIndentation.js
@@ -1,12 +1,25 @@
 import iterateJsdoc from '../iterateJsdoc';
 
+const maskExamples = (str) => {
+  const regExamples = /([ \t]+\*)[ \t]@example(?=[ \n])([\w|\W]*?\n)(?=[ \t]*\*(?:[ \t]*@|\/))/g;
+  return str.replace(regExamples, function hideCode (m, margin, code) {
+    return (new Array(code.match(/\n/g).length + 1)).join(margin + '\n');
+  });
+};
+
 export default iterateJsdoc(({
   sourceCode,
   jsdocNode,
   report,
+  context,
 }) => {
+  const options = context.options[0] || {};
+  const {
+    excludeExamples = true
+  } = options;
+
   const reg = new RegExp(/^(?:\/?\**|[ \t]*)\*[ \t]{2}/gm);
-  const text = sourceCode.getText(jsdocNode);
+  const text = excludeExamples ? maskExamples(sourceCode.getText(jsdocNode)) : sourceCode.getText(jsdocNode);
 
   if (reg.test(text)) {
     const lineBreaks = text.slice(0, reg.lastIndex).match(/\n/g) || [];
@@ -17,6 +30,16 @@ export default iterateJsdoc(({
 }, {
   iterateAllJsdocs: true,
   meta: {
+    schema: [{
+      additionalProperties: false,
+      properties: {
+        excludeExamples: {
+          default: true,
+          type: 'boolean'
+        },
+      },
+      type: 'object'
+    }],
     type: 'layout',
   },
 });
