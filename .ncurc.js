9be054e6331bd78f5be0beb4af744afa59e276dc
Brett Zamir <brettz9@yahoo.com>
2019-12-15 23:26:14 +0800

    chore: bump typescript-eslint/parser verion; ignore updates to semver (which now requires Node 10)

--- .ncurc.js
+++ .ncurc.js
@@ -2,5 +2,9 @@ module.exports = {
   // Whitelist all for checking besides `peer` which indicates
   //   somewhat older versions of `eslint` we still support even
   //   while our devDeps point to a more recent version
-  "dep": "prod,dev,optional,bundle"
+  "dep": "prod,dev,optional,bundle",
+  "reject": [
+    // Todo[engine:node@>=10.0.0]: 7.0.0 of semver has minimum of Node 10
+    "semver"
+  ]
 };
