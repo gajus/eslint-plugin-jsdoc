95533cc1fb223dc1cbef6eb96b19d223a6888a4d
Brett Zamir <brettz9@yahoo.com>
2019-12-29 17:10:29 +0800

    Testing: Global function checking

--- test/rules/assertions/requireParamName.js
+++ test/rules/assertions/requireParamName.js
@@ -35,6 +35,59 @@ export default {
     {
       code: `
           /**
+           * @param {string}
+           */
+          function quux (foo) {
+
+          }
+      `,
+      errors: [
+        {
+          line: 3,
+          message: 'There must be an identifier after @param tag.',
+        },
+      ],
+      options: [{
+        contexts: ['any'],
+      }],
+    },
+    {
+      code: `
+          /**
+           * @function
+           * @param {string}
+           */
+      `,
+      errors: [
+        {
+          line: 4,
+          message: 'There must be an identifier after @param tag.',
+        },
+      ],
+      options: [{
+        contexts: ['any'],
+      }],
+    },
+    {
+      code: `
+          /**
+           * @callback
+           * @param {string}
+           */
+      `,
+      errors: [
+        {
+          line: 4,
+          message: 'There must be an identifier after @param tag.',
+        },
+      ],
+      options: [{
+        contexts: ['any'],
+      }],
+    },
+    {
+      code: `
+          /**
            * @param foo
            */
           function quux (foo) {
@@ -70,11 +123,42 @@ export default {
     {
       code: `
           /**
+           * @param foo
+           */
+          function quux (foo) {
+
+          }
+      `,
+      options: [
+        {
+          contexts: ['any'],
+        },
+      ],
+    },
+    {
+      code: `
+          /**
            * @param {string} foo
            */
           function quux (foo) {
 
           }
+      `,
+    },
+    {
+      code: `
+          /**
+           * @function
+           * @param
+           */
+      `,
+    },
+    {
+      code: `
+          /**
+           * @callback
+           * @param
+           */
       `,
     },
   ],
--- test/rules/assertions/requireParamType.js
+++ test/rules/assertions/requireParamType.js
@@ -19,6 +19,59 @@ export default {
     {
       code: `
           /**
+           * @param foo
+           */
+          function quux (foo) {
+
+          }
+      `,
+      errors: [
+        {
+          line: 3,
+          message: 'Missing JSDoc @param "foo" type.',
+        },
+      ],
+      options: [{
+        contexts: ['any'],
+      }],
+    },
+    {
+      code: `
+          /**
+           * @function
+           * @param foo
+           */
+      `,
+      errors: [
+        {
+          line: 4,
+          message: 'Missing JSDoc @param "foo" type.',
+        },
+      ],
+      options: [{
+        contexts: ['any'],
+      }],
+    },
+    {
+      code: `
+          /**
+           * @callback
+           * @param foo
+           */
+      `,
+      errors: [
+        {
+          line: 4,
+          message: 'Missing JSDoc @param "foo" type.',
+        },
+      ],
+      options: [{
+        contexts: ['any'],
+      }],
+    },
+    {
+      code: `
+          /**
            * @arg foo
            */
           function quux (foo) {
@@ -82,6 +135,35 @@ export default {
           function quux (foo) {
 
           }
+      `,
+    },
+    {
+      code: `
+          /**
+           * @param {number} foo
+           */
+          function quux (foo) {
+
+          }
+      `,
+      options: [{
+        contexts: ['any'],
+      }],
+    },
+    {
+      code: `
+          /**
+           * @function
+           * @param foo
+           */
+      `,
+    },
+    {
+      code: `
+          /**
+           * @callback
+           * @param foo
+           */
       `,
     },
   ],
