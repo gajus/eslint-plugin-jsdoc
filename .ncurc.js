2bc91522e91ebbd4d4e76850f37fec20b57495c5
Brett Zamir <brettz9@yahoo.com>
2020-08-22 19:48:17 +0800

    chore: update devDeps
    Also lints ncurc file and avoids updating typescript until there is a ts parser bump

--- .ncurc.js
+++ .ncurc.js
@@ -4,5 +4,8 @@ module.exports = {
   // Whitelist all for checking besides `peer` which indicates
   //   somewhat older versions of `eslint` we still support even
   //   while our devDeps point to a more recent version
-  dep: 'prod,dev,optional,bundle'
+  dep: 'prod,dev,optional,bundle',
+
+  // Todo [@typescript-eslint/parser@>=4]: Not supporting typescript 4
+  reject: ['typescript'],
 };
