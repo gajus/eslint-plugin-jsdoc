08e76b12f67b1b83354f1f6c86a65a4a963286ef
Brett Zamir <brettz9@yahoo.com>
2021-02-12 14:23:50 +0800

    chore: update devDeps (besides husky which is temporarily changing its license from MIT)

--- .ncurc.js
+++ .ncurc.js
@@ -5,4 +5,7 @@ module.exports = {
   //   somewhat older versions of `eslint` we still support even
   //   while our devDeps point to a more recent version
   dep: 'prod,dev,optional,bundle',
+
+  // Todo [husky@>=6]: Might be MIT again
+  reject: ['husky'],
 };
