9f4440d76b1452dd1c9957a380ebeeb840ec51c4
Brett Zamir <brettz9@yahoo.com>
2021-11-26 18:40:54 +0800

    chore: update devDeps

--- .ncurc.js
+++ .ncurc.js
@@ -5,4 +5,10 @@ module.exports = {
   //   somewhat older versions of `eslint` we still support even
   //   while our devDeps point to a more recent version
   dep: 'prod,dev,optional,bundle',
+  reject: [
+    // Todo: When package converted to ESM
+    'decamelize',
+    'escape-string-regexp',
+    'open-editor',
+  ],
 };
