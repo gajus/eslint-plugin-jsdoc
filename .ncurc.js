462f96d27c6de0b9e913f48a4161384024da8deb
Brett Zamir <brettz9@yahoo.com>
2022-12-07 09:06:39 -0700

    chore: update devDeps.
    Also:
    - chore: drop no-longer-needed `npm-check-updates` config

--- .ncurc.js
+++ .ncurc.js
@@ -1,10 +1,6 @@
 'use strict';
 
 module.exports = {
-  // Whitelist all for checking besides `peer` which indicates
-  //   somewhat older versions of `eslint` we still support even
-  //   while our devDeps point to a more recent version
-  dep: 'prod,dev,optional,bundle',
   reject: [
     // Todo: When package converted to ESM
     'camelcase',
