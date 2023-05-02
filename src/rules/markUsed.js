aa17ecd4d5168e609cb16bd5a31aeda9286f1c49
Kræn Hansen <kraen.hansen@mongodb.com>
2023-05-02 20:42:53 +0200

    Implemented rule for non-inline tags

--- src/rules/markUsed.js
+++ src/rules/markUsed.js
@@ -1,6 +1,46 @@
 import iterateJsdoc from '../iterateJsdoc';
 
-export default iterateJsdoc(() => {}, {
+/**
+ * Extracts the type names from a type declaration string.
+ *
+ * @see https://jsdoc.app/tags-type.html
+ */
+const extractTypeNames = (type) => {
+  if (type.startsWith('(') && type.endsWith(')')) {
+    // Type union
+    return type.slice(1, type.length - 1).split('|').flatMap(extractTypeNames);
+  } else if (type.endsWith('[]')) {
+    // Arrays
+    return extractTypeNames(type.slice(0, Math.max(0, type.length - 2)));
+  } else if (type.startsWith('!') || type.startsWith('?')) {
+    // Nullable type or non-nullable type
+    return extractTypeNames(type.slice(1));
+  } else if (type.startsWith('...')) {
+    // Variable number of that type
+    return extractTypeNames(type.slice(3));
+  } else if (type.endsWith('=')) {
+    // Optional parameter
+    return extractTypeNames(type.slice(0, Math.max(0, type.length - 1)));
+  } else {
+    return [
+      type,
+    ];
+  }
+};
+
+export default iterateJsdoc(({
+  jsdoc,
+  jsdocNode,
+  context,
+}) => {
+  const sourceCode = context.getSourceCode();
+  for (const tag of jsdoc.tags) {
+    const typeNames = extractTypeNames(tag.type);
+    for (const typeName of typeNames) {
+      sourceCode.markVariableAsUsed(typeName, jsdocNode);
+    }
+  }
+}, {
   iterateAllJsdocs: true,
   meta: {
     docs: {
--- test/rules/assertions/markUsed.js
+++ test/rules/assertions/markUsed.js
@@ -1,16 +1,51 @@
 export default {
   invalid: [],
   valid: [
+    // {
+    //   code: `
+    //     const foo = "bar";
+    //     /** This thing uses {@link foo} for something */
+    //   `,
+    //   /*
+    //   rules: {
+    //     'no-unused-vars': 'error',
+    //   },
+    //   */
+    // },
     {
       code: `
-        const foo = "bar";
-        /** This thing uses {@link foo} for something */
+        class Foo {}
+        /** @param {Foo} */
+        function foo() {}
+        foo();
       `,
-      /*
       rules: {
         'no-unused-vars': 'error',
       },
-      */
+    },
+    {
+      code: `
+        class Foo {}
+        /** @returns {Foo} */
+        function foo() {}
+        foo();
+      `,
+      rules: {
+        'no-unused-vars': 'error',
+      },
+    },
+    {
+      code: `
+        class Foo {}
+        class Bar {}
+        class Baz {}
+        class Qux {}
+        /** @type {(!Foo|?Bar|...Baz|Qux[]|foo=)} */
+        let foo = null;
+      `,
+      rules: {
+        'no-unused-vars': 'error',
+      },
     },
   ],
 };
