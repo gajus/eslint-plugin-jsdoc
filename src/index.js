d2c60403bb55a14eadbf49fc9937caad14a29cde
Leonardo Gurgel <leo@gurgel.io>
2024-12-10 21:58:07 -0300

    fix(types): update configs types (#1344)
    Co-authored-by: Brett Zamir <brettz9@yahoo.com>

--- src/index.js
+++ src/index.js
@@ -59,19 +59,13 @@ import validTypes from './rules/validTypes.js';
 import { getJsdocProcessorPlugin } from './getJsdocProcessorPlugin.js';
 
 /**
+ * @typedef {"recommended" | "stylistic" | "contents" | "logical"} ConfigGroups
+ * @typedef {"" | "-typescript" | "-typescript-flavor"} ConfigVariants
+ * @typedef {"" | "-error"} ErrorLevelVariants
  * @type {import('eslint').ESLint.Plugin & {
- *   configs: Record<
- *     "recommended"|"recommended-error"|"recommended-typescript"|
- *       "recommended-typescript-error"|"recommended-typescript-flavor"|
- *       "recommended-typescript-flavor-error"|"flat/recommended"|
- *       "flat/recommended-error"|"flat/recommended-typescript"|
- *       "flat/recommended-typescript-error"|
- *       "flat/recommended-typescript-flavor"|
- *       "flat/recommended-typescript-flavor-error",
- *     import('eslint').Linter.FlatConfig
- *   >
- * }}
- */
+*   configs: Record<`flat/${ConfigGroups}${ConfigVariants}${ErrorLevelVariants}`, import('eslint').Linter.Config>
+* }}
+*/
 const index = {
   // @ts-expect-error Ok
   configs: {},
