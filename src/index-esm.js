9261da0153a6fcc76b6a87eb00907f168cfe4f38
Brett Zamir <brettz9@yahoo.com>
2025-09-09 20:57:36 +0800

    fix(typescript): attempt to fix types export (#1483)

---
+++ src/index-esm.js
@@ -0,0 +1,75 @@
+/* eslint-disable perfectionist/sort-imports -- For auto-generate; Do not remove */
+import {
+  merge,
+} from 'object-deep-merge';
+
+// BEGIN REPLACE
+import index from './index-cjs.js';
+
+// eslint-disable-next-line unicorn/prefer-export-from --- Reusing `index`
+export default index;
+// END REPLACE
+
+/* eslint-disable jsdoc/valid-types -- Bug */
+/**
+ * @type {((
+ *   cfg?: {
+ *     mergeSettings?: boolean,
+ *     config?: `flat/${import('./index-cjs.js').ConfigGroups}${import('./index-cjs.js').ConfigVariants}${import('./index-cjs.js').ErrorLevelVariants}`,
+ *     settings?: Partial<import('./iterateJsdoc.js').Settings>
+ *   }
+ * ) => import('eslint').Linter.Config)}
+ */
+/* eslint-enable jsdoc/valid-types -- Bug */
+export const jsdoc = function (cfg) {
+  /** @type {import('eslint').Linter.Config} */
+  let outputConfig = {
+    plugins: {
+      jsdoc: index,
+    },
+  };
+  if (
+    cfg?.config
+  ) {
+    // @ts-expect-error Security check
+    if (cfg.config === '__proto__') {
+      throw new TypeError('Disallowed config value');
+    }
+
+    outputConfig = index.configs[cfg.config];
+  }
+
+  outputConfig.settings = {
+    jsdoc: cfg?.mergeSettings === false ?
+      cfg.settings :
+      merge(
+        {},
+        cfg?.settings ?? {},
+        cfg?.config?.includes('recommended') ?
+          {
+            // We may need to drop these for "typescript" (non-"flavor") configs,
+            //   if support is later added: https://www.typescriptlang.org/docs/handbook/jsdoc-supported-types.html
+            structuredTags: {
+              next: {
+                required: [
+                  'type',
+                ],
+              },
+              throws: {
+                required: [
+                  'type',
+                ],
+              },
+              yields: {
+                required: [
+                  'type',
+                ],
+              },
+            },
+          } :
+          {},
+      ),
+  };
+
+  return outputConfig;
+};
