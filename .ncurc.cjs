d45a71d5ac420abe739d22e9d644740513850155
Brett Zamir <brettz9@yahoo.com>
2025-10-26 08:41:56 -0700

    fix(CJS): provide CJS version of to-valid-identiifer; fixes #1583

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -4,8 +4,6 @@ module.exports = {
   reject: [
     // Todo: When our package is converted to ESM only
     'escape-string-regexp',
-
-    // Todo: When our package is converted to ESM only
     'are-docs-informative',
   ],
 };
